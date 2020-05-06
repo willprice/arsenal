e1452cf4b519dcee04448c5b211edef511b50038
Will Price <will.price94@gmail.com>
2020-05-06 17:44:52 +0100

    Add docstring to clip_to_html

--- src/arsenal/video.py
+++ src/arsenal/video.py
@@ -9,6 +9,28 @@ from moviepy.video.VideoClip import VideoClip
 def clip_to_html(
     clip: Union[VideoClip, np.ndarray], verbose=False, fps=24, **kwargs
 ) -> str:
+    """Convert a MoviePy clip to an HTML string.
+
+    .. code-block:: python
+
+        from IPython.display import display
+        clip = ImageSequenceClip(list(np_video))
+        display(clip_to_html(clip))
+
+    Args:
+        clip: MoviePy clip.
+        verbose: Whether to print out FFmpeg information during encoding
+        fps: FPS of clip
+        **kwargs: Any kwargs to pass down to :py:func:`html_embed`
+
+    Returns:
+        String of HTML with a ``<video>`` tag and base64 encoded media.
+        Useful for use with ``IPython.display.display`` to show videos.
+
+
+
+
+    """
     if isinstance(clip, (np.ndarray, list)):
         clip = ImageSequenceClip(list(clip), fps=fps)
     assert hasattr(clip, "write_videofile")
